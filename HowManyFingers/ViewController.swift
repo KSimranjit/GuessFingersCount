5a41e01094b63b4e61bafed37ccda7e949010c52
Simranjit <simranjit2701@gmail.com>
2016-02-02 21:13:36 +1100

    Using random number generator method Also changing image of UIImageview according to the number generated

--- HowManyFingers/ViewController.swift
+++ HowManyFingers/ViewController.swift
@@ -10,6 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fingersImage: UIImageView!
+    @IBOutlet weak var fingerNumber: UITextField!
+    @IBOutlet weak var ActualResult: UILabel!
+    @IBAction func guessDone(sender: AnyObject) {
+        
+        let numberOfFingers = String(arc4random_uniform(6))
+        
+        print(numberOfFingers)
+        
+       if fingerNumber.text == nil || fingerNumber.text == ""{
+          
+            ActualResult.text = "Please enter a number"
+       }else {
+        
+        if numberOfFingers == fingerNumber.text{
+         
+            ActualResult.text = "You are right"
+        }
+        else{
+            ActualResult.text = "Wrong! It was a " + numberOfFingers
+        }
+        
+        if numberOfFingers == "0"{
+            self.fingersImage.image = UIImage(named: "NoFinger.jpg")!
+        }
+        else if numberOfFingers == "1"{
+            self.fingersImage.image = UIImage(named: "OneFinger.jpg")!
+        }
+        else if numberOfFingers == "2"{
+            self.fingersImage.image = UIImage(named: "TwoFinger.jpg")!
+        }
+        else if numberOfFingers == "3"{
+            self.fingersImage.image = UIImage(named: "ThreeFinger.jpg")!
+        }
+        else if numberOfFingers == "4"{
+            self.fingersImage.image = UIImage(named: "FourFinger.jpg")!
+        }
+        else {
+            self.fingersImage.image = UIImage(named: "FiveFinger.jpg")!
+        }
+        
+    }
+    
+        
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
